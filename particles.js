cf02e163ca825131bd0d127d098a6c3c0aabb8c2
Paul BOUSSEKEY <pboussekey@thestudnet.com>
2017-01-28 11:30:18 +0100

    particles

--- particles.js
+++ particles.js
@@ -1,8 +1,8 @@
 
 $(document).ready(function(){
-    var h = 350;
+    var h = 400;
     var w =  $("#header").outerWidth();
-    var center = [30, 0];
+    var center = [0, 0];
 
     var radius = [
         350,
@@ -11,18 +11,28 @@ $(document).ready(function(){
         150,
         100
     ];
+    
+    var positions = {
+        up : function(){
+            return [-50, w * Math.random()];            
+        },
+        right : function(){
+            return [h * Math.random(), w + 50];
+        },
+        bottom : function(){
+            return [h + 50, w * Math.random()]
+        },
+        left : function(){
+            return [h * Math.random(), -50]
+        }
+    };
+    
+    
 
     function distanceToCenter(top, left){
         var distance = Math.floor(Math.sqrt(Math.pow(left - center[1],2) + Math.pow(top - center[0], 2)));
         return distance;
     };
-
-    function makeNewPosition(){
-        var ny = Math.floor(Math.random() * h);
-        var nx = Math.floor(Math.random() * w);
-
-        return [ny,nx];    
-    }
 
     function getIntersection(particle){
         var cx = particle.x;
@@ -30,28 +40,26 @@ $(document).ready(function(){
         var currentDistance = distanceToCenter(cy, cx);
         var nx = Math.floor(cx + (center[1] - cx) * (Math.abs(currentDistance - particle.orbit) / currentDistance));
         var ny = Math.floor(cy + (center[0] - cy) * (Math.abs(currentDistance - particle.orbit) / currentDistance));
-        console.log(radius, cx, cy, currentDistance, nx, ny, distanceToCenter(ny, nx));
         return [ny , nx];
 
     }
 
-    function animateParticle(particle){
+    function animateParticle(particle, destination){
         if(!particle.orbit){
+            particle.rotation = (Math.random() - 0.5) * 720;
+            particle.currentRotation = particle.rotation * 2;
             particle.duration = Math.max(3000, Math.random() * 15000);
-            newq = makeNewPosition();
-            var distance = distanceToCenter(newq[0], newq[1]);
+            var distance = distanceToCenter(destination[0], destination[1]);
             radius.forEach(function(r){
                if(r >= distance && Math.random() >= 0.5){
                    particle.orbit = r;
                } 
             });
             if(particle.orbit){
-                newq = getIntersection(particle);
-                particle.rotation = (Math.random() - 0.5) * 720;
-                particle.currentRotation = particle.rotation * 2;
+                destination = getIntersection(particle);
             }
-            particle.x = newq[1];
-            particle.y = newq[0];
+            particle.x = destination[1];
+            particle.y = destination[0];
         }
         else{
             particle.currentRotation = particle.currentRotation + particle.rotation;
@@ -59,7 +67,7 @@ $(document).ready(function(){
         
         particle.animate({  opacity : 1 }, {
         step : function(){
-            $(this).css(
+            $(particle).css(
                 { transform: (particle.currentRotation ? "rotate(" + (-particle.currentRotation) + "deg)" : "") 
                     + "translate(-" + particle.x + "px, " + particle.y + "px) " 
                     + (particle.currentRotation ? "rotate(" + particle.currentRotation + "deg)" : ""), 
@@ -67,7 +75,24 @@ $(document).ready(function(){
         },
         duration: particle.duration,
         complete:function(){
-            animateParticle(particle);  
+            if(!particle.orbit){
+                destroyParticle(particle);
+            }
+            else{
+                if(!particle.lifespan){
+                    particle.removeClass("small");
+                    particle.removeClass("large-border");
+                    var subparticle = $(document.createElement( "div" ));
+                    subparticle.addClass("particle");
+                    subparticle.addClass("small");
+                    subparticle.addClass(particle.hasClass("gray") ? "gray" : "");
+                    subparticle.addClass("large-border");
+                    particle.append(subparticle);
+                    
+                    particle.lifespan = setTimeout(function(){ destroyParticle(particle);},30000);
+                }
+                animateParticle(particle);
+            }
         } });
 
     };
@@ -85,16 +110,18 @@ $(document).ready(function(){
         particle.addClass(Math.random() >= 0.5 ? "transparent" : "");
         particle.addClass(Math.random() >= 0.5 ? "large-border" : "");
         particle.addClass(Math.random() >= 0.5 ? "small" : "");
-        var newq = makeNewPosition();
-        particle.css({ transform: "translate(-" + newq[1] + "px, " + newq[0] + "px)", opacity : 0 });
-        particle.x = newq[1];
-        particle.y = newq[0];
-        $( "body" ).append(particle);
-        animateParticle(particle); 
-        setTimeout(function(){ destroyParticle(particle);},30000);
+        var source=  Object.keys(positions)[Math.floor(Math.random() * 4)];
+        var destination = Object.keys(positions).filter(function(p){ return p !== source; })[Math.floor(Math.random() * 3)];
+        source = positions[source]();
+        destination = positions[destination]();
+        particle.css({ transform: "translate(-" + source[1] + "px, " + source[0] + "px)", opacity : 0 });
+        particle.x = source[1];
+        particle.y = source[0];
+        $( "#animation-layout" ).append(particle);
+        animateParticle(particle, destination); 
     };
 
-    for(var i = 0; i < 10; i++){
+    for(var i = 0; i < 15; i++){
        setTimeout(generateParticle, Math.random() * 5000);
     }
     
