cd8ef10b6da4df9c92c5a371374e578158b43117
Paul BOUSSEKEY <pboussekey@thestudnet.com>
2017-02-06 10:56:36 +0100

    test css ie

---
+++ src/app/shared/dropdown/dropdown.directive.ts
@@ -0,0 +1,22 @@
+import { Directive, HostBinding, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[app-dropdown]',
+})
+export class DropdownDirective {
+    
+    @HostBinding('class.open') get opened() {
+        return this.isOpen;
+    }
+    
+    @HostListener('click') open(){
+        this.isOpen = !this.isOpen;
+    }
+    
+    @HostListener('mouseleave') close(){
+        this.isOpen = false;
+    }    
+    
+    private isOpen = false;
+
+}
