d32a00c7758d271fb26aed13e0c08bf9c7470ec4
Paul BOUSSEKEY <pboussekey@thestudnet.com>
2017-02-09 14:27:01 +0100

    Product responsive

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -11,15 +11,27 @@ import { Circle } from 'app/shared/planetarium/planetarium.component';
 })
 export class ProductComponent implements OnInit{
     center : number[] = [0,375];
-    circles : Circle[] = [
-        { opacity: 1, radius: 80, shadow : "-5px 5px 40px 0px rgba(0,0,0,0.06)"  },
-        { opacity: 0.9, radius: 145, shadow : "-5px 5px 40px 0px rgba(0,0,0,0.06)"  },
-        { opacity: 0.8, radius: 210, shadow : "-5px 5px 40px 0px rgba(0,0,0,0.06)"  },
-        { opacity: 0.7, radius: 275, shadow : "-5px 5px 40px 0px rgba(0,0,0,0.06)"  },
-        { opacity: 0.6, radius: 350, shadow : "-5px 5px 40px 0px rgba(0,0,0,0.06)"  }
+    height : number = 800;
+    circles : Circle[];
+    circles_large : Circle[] = [
+        { opacity: 0.9, radius: 80, shadow : "-5px 5px 40px 0px rgba(0,0,0,0.06)"  },
+        { opacity: 0.8, radius: 145, shadow : "-5px 5px 40px 0px rgba(0,0,0,0.06)"  },
+        { opacity: 0.7, radius: 210, shadow : "-5px 5px 40px 0px rgba(0,0,0,0.06)"  },
+        { opacity: 0.6, radius: 275, shadow : "-5px 5px 40px 0px rgba(0,0,0,0.06)"  },
+        { opacity: 0.5, radius: 350, shadow : "-5px 5px 40px 0px rgba(0,0,0,0.06)"  }
+    ];
+    circles_small : Circle[] = [
+        { opacity: 0.9, radius: 50, shadow : "-5px 5px 40px 0px rgba(0,0,0,0.06)"  },
+        { opacity: 0.8, radius: 100, shadow : "-5px 5px 40px 0px rgba(0,0,0,0.06)"  },
+        { opacity: 0.7, radius: 150, shadow : "-5px 5px 40px 0px rgba(0,0,0,0.06)"  },
+        { opacity: 0.6, radius: 200, shadow : "-5px 5px 40px 0px rgba(0,0,0,0.06)"  },
+        { opacity: 0.5, radius: 240, shadow : "-5px 5px 40px 0px rgba(0,0,0,0.06)"  }
     ];
     onResize(){
-        this.center[0] =  document.querySelector('[app-root]').clientWidth / 2 ;
+        var width = document.querySelector('[app-root]').clientWidth;
+        this.circles = width >= 1023 ? this.circles_large : this.circles_small;
+        this.height = width >= 1023 ? 800 : 600;
+        this.center =  [width / 2, this.height / 2];
     }
     ngOnInit(){
        this.onResize();
