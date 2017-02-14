59a9d2bb7e6b96540d6d3509b8a030018582f06a
Paul BOUSSEKEY <pboussekey@thestudnet.com>
2017-02-14 14:32:37 +0100

    style mobile fix

--- src/mail.php
+++ src/mail.php
@@ -2,6 +2,7 @@
 $_POST = json_decode(file_get_contents('php://input'), true);
 return mail('pboussekey@thestudnet.com', 
             'Demande de compte de test', 
-            "Institution : ".$_POST['institution'].
-            "\r\nEmail : ".$_POST['email'].
-            "\r\nPassword : ".$_POST['password']);
+            "Domain : ".getenv(DOMAIN_NAME).
+            "\r\nInstitution : ".strip_tags(trim($_POST['institution'])).
+            "\r\nEmail : ".strip_tags( trim($_POST['email'])).
+            "\r\nPassword : ".strip_tags(trim($_POST['password'])));
