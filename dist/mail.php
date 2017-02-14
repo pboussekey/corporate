9d7f5c7e584aa6bda1c8866e54903c8bc584fba3
Paul BOUSSEKEY <pboussekey@thestudnet.com>
2017-02-14 11:40:38 +0100

    mail.php

--- dist/mail.php
+++ dist/mail.php
@@ -1,4 +1,5 @@
 <?php 
+$_POST = json_decode(file_get_contents('php://input'), true);
 var_dump($_POST);
 return mail('pboussekey@thestudnet.com', 'Demande de compte de test', "Institution : ".
     filter_input(INPUT_POST,'institution')."\r\nEmail : ".
