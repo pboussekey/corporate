6c842c3af4fef6c2afe4abbb95e5d94b958be160
Paul BOUSSEKEY <pboussekey@thestudnet.com>
2017-02-17 16:03:16 +0100

    Fix mail.php

--- dist/mail.php
+++ dist/mail.php
@@ -1,9 +1,10 @@
 <?php 
 $_POST = json_decode(file_get_contents('php://input'), true);
-return mail('pboussekey@thestudnet.com', 
+echo mail('pboussekey@thestudnet.com', 
             'Demande de compte de test', 
             "Domain : ".$_SERVER['HTTP_HOST'].
             "\r\nFirst name : ".strip_tags(trim($_POST['firstName'])).
             "\r\nLast name : ".strip_tags(trim($_POST['lastName'])).
             "\r\nInstitution : ".strip_tags(trim($_POST['institution'])).
             "\r\nEmail : ".strip_tags( trim($_POST['email'])));
+
