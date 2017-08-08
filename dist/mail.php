1934ed97dd960cf9e02f93568739f295b8b1d3f5
Paul BOUSSEKEY <pboussekey@thestudnet.com>
2017-08-08 10:31:34 +0200

    add ip in mail

--- dist/mail.php
+++ dist/mail.php
@@ -3,6 +3,7 @@ $_POST = json_decode(file_get_contents('php://input'), true);
 echo mail('pboussekey@thestudnet.com, xhoang@thestudnet.com, ssayegh@thestudnet.com, sbendacha@thestudnet.com',
             'Demande de compte de test', 
             "Domain : ".$_SERVER['HTTP_HOST'].
+            "IP : ".$_SERVER["REMOTE_ADDR"].
             "\r\nFirst name : ".strip_tags(trim($_POST['firstName'])).
             "\r\nLast name : ".strip_tags(trim($_POST['lastName'])).
             "\r\nInstitution : ".strip_tags(trim($_POST['institution'])).
